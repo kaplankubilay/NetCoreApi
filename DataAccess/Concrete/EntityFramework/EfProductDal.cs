ba46120203c653a6e2734ba4d6f823cd52c59348
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 09:45:10 +0300

    RepositoryBase nesnesi oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //EfEntityRepositoryBase.cs içerisinde implemente edilerek burada kullanıldı.
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+
+    }
+}

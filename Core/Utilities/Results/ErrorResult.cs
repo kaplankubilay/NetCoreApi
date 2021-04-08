0065dae9c15c256042eb434f8f0992574b4e27f2
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 10:20:48 +0300

    İşlem BAŞARILI-HATA sisteminin alt yapısının oluşturulması.2

---
+++ Core/Utilities/Results/ErrorResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorResult : Result
+    {
+        public ErrorResult(bool success, string message) : base(false, message)
+        {
+        }
+
+        public ErrorResult(bool success) : base(false)
+        {
+        }
+
+        public ErrorResult(string message) : base(false)
+        {
+        }
+    }
+}

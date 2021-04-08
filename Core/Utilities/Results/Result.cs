8ecbe4ea95d701e5995717d6e094f382d40da556
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 10:14:41 +0300

    İşlem BAŞARILI-HATA sisteminin alt yapısının oluşturulması.

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        /// <summary>
+        /// "this(success)" ifadesi iki ctor uda set etmenin kısa yoludur.
+        /// </summary>
+        /// <param name="success"></param>
+        /// <param name="message"></param>
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}

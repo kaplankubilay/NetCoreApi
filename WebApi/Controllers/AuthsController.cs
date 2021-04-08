a0627d0d3ce23da4d0d6ae95343c7ccd7774163b
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 16:53:54 +0300

    Authentication Api(controller) ın yapılması.

---
+++ WebApi/Controllers/AuthsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Dtos;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthsController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthsController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UserForLoginDto userForLoginDto)
+        {
+            var userToLogin = _authService.Login(userForLoginDto);
+
+            if (!userToLogin.Success)
+            {
+                return BadRequest(userToLogin.Message);
+            }
+
+            var result = _authService.CreateAccessToken(userToLogin.Data);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserForRegisterDto userForRegisterDto)
+        {
+            var userExist = _authService.UserExists(userForRegisterDto.EMail);
+
+            if (!userExist.Success)
+            {
+                return BadRequest(userExist.Message);
+            }
+
+            var registerResult = _authService.Register(userForRegisterDto, userForRegisterDto.Password);
+            var result = _authService.CreateAccessToken(registerResult.Data);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}

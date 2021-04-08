cd7ac8e45dd92b6a1efaef85ebf2d95e6db08b3a
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 11:31:15 +0300

    Api de ProductController implemente edilmesi ve projeye AUTOFACT tool unun configure edilmesi.

---
+++ Business/DependencyReselvers/AutoFact/AutofactBusinessModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyReselvers.AutoFact
+{
+    public class AutofactBusinesModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.DependencyReselvers.AutoFact;
 
 namespace WebApi
 {
@@ -18,6 +21,11 @@ namespace WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>(builder =>
+                {
+                    builder.RegisterModule(new AutofactBusinesModule());
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();

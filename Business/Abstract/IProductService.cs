357f4914aa31ae25ead6d864a682a3ab3cb00938
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 10:03:21 +0300

    DependencyInjection yapılandırılması.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(int productId);
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productDal.GetList().ToList();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            return _productDal.GetList(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}

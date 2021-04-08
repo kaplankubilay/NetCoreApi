d4171541b782e1ca69ba852e9e2aa2a016cb6769
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 16:23:28 +0300

    Access Token oluşturacak nesnenin yazılması.

---
+++ Core/Utilities/Security/JWT/TokenOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    public class TokenOptions
+    {
+        /// <summary>
+        /// Kullanıcılar
+        /// </summary>
+        public string Audience { get; set; }
+
+        /// <summary>
+        /// İmzalayanlar
+        /// </summary>
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// Token süresi(Dk)
+        /// </summary>
+        public int AccessTokenExpiration { get; set; }
+
+        public string SecurityKey { get; set; }
+
+    }
+}

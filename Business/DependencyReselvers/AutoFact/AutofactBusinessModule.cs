d9e9fcec7796d4f9fb0392aa8c20cbbea594f8cd
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 17:38:25 +0300

    JWT autofact te map edildi.

--- Business/DependencyReselvers/AutoFact/AutofactBusinessModule.cs
+++ Business/DependencyReselvers/AutoFact/AutofactBusinessModule.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using Business.Abstract;
 using Business.Concrete;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -21,6 +22,9 @@ namespace Business.DependencyReselvers.AutoFact
 
             builder.RegisterType<UserManager>().As<IUserService>();
             builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
 
 
         }

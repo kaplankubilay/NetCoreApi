a6a3e8e2e70c6c1672817469e9732470bf653a97
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 09:32:24 +0300

    Entityframework ve ConnectionString yapılandırması.

---
+++ DataAccess/Concrete/EntityFramework/Context/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=Northwind; Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}

b57c822792ef34ef4781c05b73646ee4b9e10c59
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-08 16:42:54 +0300

    JWT Token ile Authentication yazıldı.("Login","UserExists","CreateAccessToken","Register")

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.JWT;
+using Entities.Dtos;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        /// <summary>
+        /// Kullanıcı kayıt iişlemi
+        /// </summary>
+        /// <param name="userForRegisterDto"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
+
+        /// <summary>
+        /// Kullanıcı login işlemi
+        /// </summary>
+        /// <param name="userForLoginDto"></param>
+        /// <returns></returns>
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+
+        /// <summary>
+        /// Daha önce oluşturulan mail kontrolü/kullanıcı var mı?
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        IResult UserExists(string email);
+
+        /// <summary>
+        /// Erişim için oluşturulan token.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        IDataResult<AccessToken> CreateAccessToken(User user);
+    }
+}
---
+++ Core/Utilities/Hashing/HashingHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Hashing
+{
+    public class HashingHelper
+    {
+        /// <summary>
+        /// Password hash i oluşturacak.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="passwordHash"></param>
+        /// <param name="passwordSalt"></param>
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        /// <summary>
+        /// DB deki değer ile kullanıcının gönderdiği hash karşılaştırılıyor.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="passwordHash"></param>
+        /// <param name="passwordSalt"></param>
+        /// <returns></returns>
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}

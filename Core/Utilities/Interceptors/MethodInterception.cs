81ac64cc180e3b75a33ca7ac98d6f75eb81ae113
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-09 16:18:45 +0300

    Metot interception alt yapısının oluşturulması.

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.Core.Interceptor;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        /// <summary>
+        /// metodun önünde çalış.
+        /// </summary>
+        /// <param name="invocation"></param>
+        protected virtual void OnBefore(IInvocation invocation)
+        {
+
+        }
+
+        /// <summary>
+        /// metodun arkasından çalış.
+        /// </summary>
+        /// <param name="invocation"></param>
+        protected virtual void OnAfter(IInvocation invocation)
+        {
+
+        }
+
+        /// <summary>
+        /// Metod hatalıysa çalış.
+        /// </summary>
+        /// <param name="invocation"></param>
+        protected virtual void OnException(IInvocation invocation, System.Exception e)
+        {
+
+        }
+
+        /// <summary>
+        /// Metod başarılıysa çalış.
+        /// </summary>
+        /// <param name="invocation"></param>
+        protected virtual void OnSuccess(IInvocation invocation)
+        {
+
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation, e);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.Core.Interceptor;
+
+namespace Core.Utilities.Interceptors
+{
+    /// <summary>
+    /// Classlarda, Metodlarda, Birden fazla yerde kullanılabilmesini,Inherit edildiği yerde kullanılabilmesi sağlandı.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        /// <summary>
+        /// Attribute ün çalışma önceliğini belirleyen property.
+        /// </summary>
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+        }
+    }
+}

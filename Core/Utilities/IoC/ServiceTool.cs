9ed4a3d3a77055bf41ab3e0c67ce067b92176b9b
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-09 17:32:59 +0300

    .NetCore un kendi servislerine erişim sağlayacak class eklendi.

---
+++ Core/Utilities/IoC/ServiceTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Utilities.IoC
+{
+    /// <summary>
+    /// "ServiceTool" .NetCore un kendi servislerine erişim için kullanılacak.
+    /// </summary>
+    public static class ServiceTool
+    {
+        public static IServiceProvider ServiceProvider { get; set; }
+
+        public static IServiceCollection Create(IServiceCollection services)
+        {
+            ServiceProvider = services.BuildServiceProvider();
+            return services;
+        }
+    }
+}

cd200a10f940a242569cf847315d9b621f5e7ec1
kaplankubilay <67460042+kaplankubilay@users.noreply.github.com>
2021-04-09 14:33:25 +0300

    Authentication ile ilgili "StartUp" kofigürasyonu yapıldı.

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -10,6 +10,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebApi
 {
@@ -26,6 +30,27 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //angular için cors configurasyonu yapýldý.
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin", builder => builder.WithOrigins("http://localhost:4200"));
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,10 +61,17 @@ namespace WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+            //Eger talep 4200 den gelirse her türlü talebe cevap ver.
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            //bir yere girmek için kullanýlan anahtardýr.
+            app.UseAuthentication();
+
+            //yetki ile neler yapýlabileceðini belirler.
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
